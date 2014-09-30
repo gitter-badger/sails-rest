09e0e36cca3293e6ba6135302f3f8a57f21d24c0
Roman Gafurov <gafurov.roma89@gmail.com>
2014-10-01 00:21:47 +0400

    Organize code like new specification of adapters.

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -1,23 +1,40 @@
-var tests = require('waterline-adapter-tests'),
-    adapter = require('../../SailsRest'),
-    mocha = require('mocha'),
-    log = require('captains-log')(),
-    app = require('../support/app');
+/**
+ * Run integration tests
+ *
+ * Uses the `waterline-adapter-tests` module to
+ * run mocha tests against the appropriate version
+ * of Waterline.  Only the interfaces explicitly
+ * declared in this adapter's `package.json` file
+ * are tested. (e.g. `queryable`, `semantic`, etc.)
+ */
+
+
+/**
+ * Module dependencies
+ */
+
+var util = require('util');
+var mocha = require('mocha');
+var log = new (require('captains-log'))();
+var TestRunner = require('waterline-adapter-tests');
+var Adapter = require('../../SailsRest');
+var app = require('../support/app');
 
 
 // Grab targeted interfaces from this adapter's `package.json` file:
-var package = {},
-  interfaces = [];
+var package = {};
+var interfaces = [];
 try {
-  package = require('../../package.json');
-  interfaces = package.waterlineAdapter.interfaces;
-} catch (e) {
-  throw new Error(
-    '\n' +
-    'Could not read supported interfaces from `waterlineAdapter.interfaces`' + '\n' +
+    package = require('../../package.json');
+    interfaces = package['waterlineAdapter'].interfaces;
+}
+catch (e) {
+    throw new Error(
+    '\n'+
+    'Could not read supported interfaces from `waterlineAdapter.interfaces`'+'\n' +
     'in this adapter\'s `package.json` file ::' + '\n' +
     util.inspect(e)
-  );
+    );
 }
 
 
@@ -30,39 +47,46 @@ log('Latest draft of Waterline adapter interface spec:');
 log('http://links.sailsjs.org/docs/plugins/adapters/interfaces');
 console.log();
 
-
-
-
-var config = {
-  hostname: 'localhost',
-  port: 3000,
-  pathname: '/api/v1'
-};
-
 app.listen(3000);
-var suite = new tests({
-  adapter: adapter,
-  config: config,
 
-  // The set of adapter interfaces to test against.
-  // (grabbed these from this adapter's package.json file above)
-  interfaces: interfaces
+/**
+ * Integration Test Runner
+ *
+ * Uses the `waterline-adapter-tests` module to
+ * run mocha tests against the specified interfaces
+ * of the currently-implemented Waterline adapter API.
+ */
+new TestRunner({
 
-  // Most databases implement 'semantic' and 'queryable'.
-  //
-  // As of Sails/Waterline v0.10, the 'associations' interface
-  // is also available. If you don't implement 'associations',
-  // it will be polyfilled for you by Waterline core. The core
-  // implementation will always be used for cross-adapter / cross-connection
-  // joins.
-  //
-  // In future versions of Sails/Waterline, 'queryable' may be also
-  // be polyfilled by core.
-  //
-  // These polyfilled implementations can usually be further optimized at the
-  // adapter level, since most databases provide optimizations for internal
-  // operations.
-  //
-  // Full interface reference:
-  // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
+    // Load the adapter module.
+    adapter: Adapter,
+
+    // Default adapter config to use.
+    config: {
+      hostname: 'localhost',
+      port: 3000,
+      pathname: '/api/v1'
+    },
+
+    // The set of adapter interfaces to test against.
+    // (grabbed these from this adapter's package.json file above)
+    interfaces: interfaces
+
+    // Most databases implement 'semantic' and 'queryable'.
+    //
+    // As of Sails/Waterline v0.10, the 'associations' interface
+    // is also available.  If you don't implement 'associations',
+    // it will be polyfilled for you by Waterline core.  The core
+    // implementation will always be used for cross-adapter / cross-connection
+    // joins.
+    //
+    // In future versions of Sails/Waterline, 'queryable' may be also
+    // be polyfilled by core.
+    //
+    // These polyfilled implementations can usually be further optimized at the
+    // adapter level, since most databases provide optimizations for internal
+    // operations.
+    //
+    // Full interface reference:
+    // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
 });
